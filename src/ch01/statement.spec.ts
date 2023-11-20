4308381b5bf5cf11270d7dea178fb541cf166ba4
이혜천 <rainbow880616@gmail.com>
2023-11-20 18:08:47 +0900

    #ch1 - 테스트 코드 추가.

--- src/ch01/statement.spec.ts
+++ src/ch01/statement.spec.ts
@@ -5,6 +5,10 @@ import {invoices, plays} from "./data";
 
 test("statement", () => {
   const result = statement(invoices[0], plays);
-  console.log(result);
-  // expect("").toBe("");
+  expect(result).toContain("청구 내역 (고객명: BigCo)")
+  expect(result).toContain("Hamlet: $650.00 (55석)")
+  expect(result).toContain("As You Like It: $580.00 (35석)")
+  expect(result).toContain("Othello: $500.00 (40석)")
+  expect(result).toContain("총액: $1,730.00")
+  expect(result).toContain("적립 포인트: 47점")
 })
