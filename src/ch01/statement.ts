44181f28773732f9e3ce3835b3aac67cc02a8d2b
이혜천 <rainbow880616@gmail.com>
2023-11-20 18:41:34 +0900

    #ch1 - 반복문 쪼개기.

--- src/ch01/statement.ts
+++ src/ch01/statement.ts
@@ -1,20 +1,18 @@
 import {Invoice, Performance, Plays} from "./types";
 
 export function statement(invoice: Invoice, plays: Plays) {
-  let totalAmount = 0;
-  let volumeCredits = 0;
+  // let totalAmount = 0;
+
   let result = `청구 내역 (고객명: ${invoice.customer})\n`;
 
 
   for (const performance of invoice.performances) {
-    volumeCredits += volumeCreditsFor(performance);
 
     result += ` ${playFor(performance).name}: ${usd(amountFor(performance))} (${performance.audience}석)\n`;
-    totalAmount += amountFor(performance);
   }
 
-  result += `총액: ${usd(totalAmount)}\n`;
-  result += `적립 포인트: ${volumeCredits}점\n`;
+  result += `총액: ${usd(totalAmount())}\n`;
+  result += `적립 포인트: ${totalVolumeCredits()}점\n`;
   return result;
 
   function usd(number: number) {
@@ -22,6 +20,23 @@ export function statement(invoice: Invoice, plays: Plays) {
       style: "currency", currency: "USD",
       minimumFractionDigits: 2
     }).format(number / 100);
+  }
+
+  function totalAmount() {
+    let result = 0;
+    for (const performance of invoice.performances) {
+
+      result += amountFor(performance);
+    }
+    return result;
+  }
+
+  function totalVolumeCredits() {
+    let result = 0;
+    for (const performance of invoice.performances) {
+      result += volumeCreditsFor(performance);
+    }
+    return result;
   }
 
   function volumeCreditsFor(performance: Performance) {
