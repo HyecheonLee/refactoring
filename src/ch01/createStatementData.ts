92921c07f79658cc21b2c27b2b25852883bebf75
이혜천 <rainbow880616@gmail.com>
2023-11-20 20:36:12 +0900

    #ch1 - 다형성 작업.

--- src/ch01/createStatementData.ts
+++ src/ch01/createStatementData.ts
@@ -1,7 +1,65 @@
 import {Invoice, Performance, PerformanceDetail, Play, Plays, StatementData} from "./types";
 
+abstract class PerformanceCalculator {
+  protected constructor(protected performance: Performance) {
+  }
+
+  abstract calcAmount(): number;
+
+  calcVolumeCredits(): number {
+    return Math.max(this.performance.audience - 30, 0);
+  }
+
+  static create(performance: Performance, play: Play) {
+    switch (play.type) {
+      case "tragedy":
+        return new TragedyCalculator(performance);
+      case "comedy":
+        return new ComedyCalculator(performance);
+    }
+  }
+}
+
+class TragedyCalculator extends PerformanceCalculator {
+  constructor(performance: Performance) {
+    super(performance);
+  }
+
+  calcAmount(): number {
+    let result = 40000;
+    result = 40000;
+    if (this.performance.audience > 30) {
+      result += 1000 * (this.performance.audience - 30);
+    }
+    return result;
+  }
+}
+
+class ComedyCalculator extends PerformanceCalculator {
+  constructor(performance: Performance) {
+    super(performance);
+  }
+
+  calcAmount(): number {
+    let result = 30000;
+    if (this.performance.audience > 20) {
+      result += 10000 + 500 * (this.performance.audience - 20);
+      result += 300 * this.performance.audience;
+    }
+    return result;
+  }
+
+  calcVolumeCredits(): number {
+    let result = 0;
+    result += super.calcVolumeCredits();
+    result += Math.floor(this.performance.audience / 5);
+    return result;
+  }
+}
+
 export default function createStatementData(invoice: Invoice, plays: Plays) {
   const performance = invoice.performances.map(value => enrichPerformance(value, plays));
+
   const statementData: StatementData = {
     customer: invoice.customer,
     performance: performance,
@@ -14,44 +72,16 @@ export default function createStatementData(invoice: Invoice, plays: Plays) {
 function enrichPerformance(performance: Performance, plays: Plays): PerformanceDetail {
   const newPerformance = {...performance} as Performance;
   const play = playFor(newPerformance, plays);
+  const calculator = PerformanceCalculator.create(performance, play);
   return {
     ...newPerformance,
     play: play,
-    amount: amountFor(newPerformance, play),
-    volumeCredits: volumeCreditsFor(newPerformance, play),
-  }
-
-  function amountFor(performance: Performance, play: Play) {
-    let result = 0;
-    switch (play.type) {
-      case "tragedy": // 비극
-        result = 40000;
-        if (performance.audience > 30) {
-          result += 1000 * (performance.audience - 30);
-        }
-        break;
-      case "comedy": // 희극
-        result = 30000;
-        if (performance.audience > 20) {
-          result += 10000 + 500 * (performance.audience - 20);
-          result += 300 * performance.audience;
-          break;
-        }
-      default:
-        throw new Error(`알 수 없는 장르: ${play.type}`);
-    }
-    return result;
+    amount: calculator.calcAmount(),
+    volumeCredits: calculator.calcVolumeCredits(),
   }
 
   function playFor(performance: Performance, plays: Plays) {
     return plays[performance.playID];
-  }
-
-  function volumeCreditsFor(performance: Performance, play: Play) {
-    let result = 0;
-    result += Math.max(performance.audience - 30, 0);
-    if ("comedy" === play.type) result += Math.floor(performance.audience / 5);
-    return result;
   }
 }
 
