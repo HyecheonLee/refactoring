0c6ce1ea1ab4d6c5eda20b198cac7f45dbca35e9
이혜천 <rainbow880616@gmail.com>
2023-11-29 21:37:36 +0900

    ch06 extractVariable - 객체

--- src/ch06/extractVariable.ts
+++ src/ch06/extractVariable.ts
@@ -9,3 +9,29 @@ function prince(order: Order) {
   const shipping = Math.min(basePrice * 0.1, 100);
   return basePrice - quantityDiscount + shipping;
 }
+
+class Order2 {
+  quantity: number;
+  itemPrice: number;
+
+  constructor(quantity: number, itemPrice: number) {
+    this.quantity = quantity;
+    this.itemPrice = itemPrice;
+  }
+
+  get basePrice() {
+    return this.quantity * this.itemPrice;
+  }
+
+  get quantityDiscount() {
+    return Math.max(0, this.quantity - 500) * this.itemPrice * 0.05;
+  }
+
+  get shipping() {
+    return Math.min(this.basePrice * 0.1, 100);
+  }
+
+  get price() {
+    return this.basePrice - this.quantityDiscount + this.shipping;
+  }
+}
